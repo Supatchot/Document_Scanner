0725daf431df558bf4f9ae28dd9470875bb15cb7
Supatchot <Supatchot21@gmail.com>
2026-03-07 22:56:53 +0700

    No exe

--- dist/UI/_internal/cv2/config-3.13.py
+++
@@ -1,3 +0,0 @@
-PYTHON_EXTENSIONS_PATHS = [
-    os.path.join('C:/Users/Jerry1/miniconda3/envs/DocScannerEnv2/Lib/site-packages/cv2', 'python-3.13')
-] + PYTHON_EXTENSIONS_PATHS
--- dist/UI/_internal/cv2/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BINARIES_PATHS = [
-    os.path.join('C:/Users/Jerry1/miniconda3/envs/DocScannerEnv2/Library', 'bin')
-] + BINARIES_PATHS
